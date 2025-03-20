081815bdc2b5e8423b0aba307a4bddfa340e2df4
Madin <Alberto.Madin@nielseniq.com>
2025-03-19 18:10:29 -0600

    Cambios ejecutados en la App

--- App.R
+++ App.R
@@ -2662,3 +2662,5 @@ output$interactivo_plot_a = renderGirafe({library(ggiraph) # install.packages('g
 
 # Ejecutar la aplicación Shiny
 shinyApp(ui = ui, server = server)
+
+system("git remote set-url origin https://github.com/AlbertoMadinRivera/GraphGallery_R.git")
